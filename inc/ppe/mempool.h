d97122c23e58972c1cb5ac93ab8853a65befb771
LIANG Tao <amethyst.black@gmail.com>
2018-09-09 10:01:11 +0800

    Add new interfaces and fix some typo.

--- inc/ppe/mempool.h
+++ inc/ppe/mempool.h
@@ -9,35 +9,39 @@ extern "C"
 {
 #endif
 
+/* ---- Types ---- */
+
 struct PPE_MEMPOOL;
 typedef struct PPE_MEMPOOL * ppe_mempool;
 typedef struct PPE_MEMPOOL ** ppe_mempool_itf;
 
-typedef void * (*ppe_mp_allocate_vfn)(ppe_mempool_itf restrict itf, ppe_size size);
-typedef void * (*ppe_mp_reallocate_vfn)(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size);
-typedef void (*ppe_mp_deallocate_vfn)(ppe_mempool_itf restrict itf, void * restrict ptr);
+typedef void * (*ppe_mp_malloc_vfn)(ppe_mempool_itf restrict itf, ppe_size size);
+typedef void * (*ppe_mp_realloc_vfn)(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size);
+typedef void (*ppe_mp_free_vfn)(ppe_mempool_itf restrict itf, void * restrict ptr);
 
-PPE_API ppe_mempool ppe_mp_create(ppe_mp_allocate_vfn restrict alloc, ppe_mp_reallocate_vfn restrict realloc, ppe_mp_free_vfn restrict free);
-PPE_API void ppe_mp_destroy(ppe_mempool restrict mp);
+/* ---- Preset Values ---- */
 
-PPE_API void * ppe_mp_allocate(ppe_mempool_itf restrict itf, ppe_size size);
-PPE_API void * ppe_mp_reallocate(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size);
-PPE_API void ppe_mp_deallocate(ppe_mempool_itf restrict itf, void * restrict ptr);
+const ppe_mempool_itf PPE_MP_GLOBAL_ALLOCATOR = NULL:
 
-static inline void * ppe_mp_malloc(ppe_size size)
-{
-    return ppe_mp_allocate(NULL, size);
-}
+/* ---- Functions ---- */
 
-static inline void * ppe_mp_realloc(void * restrict ptr, ppe_size size)
-{
-    return ppe_mp_reallocate(NULL, ptr, size);
-}
+PPE_API extern ppe_mempool ppe_mp_create(ppe_mp_malloc_vfn restrict malloc, ppe_mp_realloc_vfn restrict realloc, ppe_mp_free_vfn restrict free);
+PPE_API extern void ppe_mp_destroy(ppe_mempool restrict mp);
 
-static inline void ppe_mp_free(void * restrict ptr)
-{
-    return ppe_mp_deallocate(NULL, ptr);
-}
+PPE_API extern void * ppe_mp_malloc_from(ppe_mempool_itf restrict itf, ppe_size size);
+PPE_API extern void * ppe_mp_calloc_from(ppe_mempool_itf restrict itf, ppe_size num, ppe_size size);
+PPE_API extern void * ppe_mp_realloc_from(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size);
+PPE_API extern void ppe_mp_free_to(ppe_mempool_itf restrict itf, void * restrict ptr);
+
+PPE_API extern ppe_mempool_itf ppe_mp_set_default(ppe_mempool_itf restrict itf);
+PPE_API extern ppe_mempool_itf ppe_mp_get_default(void);
+
+/* ---- Wrappers ---- */
+
+PPE_API extern void * ppe_mp_malloc(ppe_size size);
+PPE_API extern void * ppe_mp_calloc(ppe_size num, ppe_size size);
+PPE_API extern void * ppe_mp_realloc(void * restrict ptr, ppe_size size)
+PPE_API extern void ppe_mp_free(void * restrict ptr);
 
 #ifdef __cplusplus
 }
--- src/mempool.c
+++ src/mempool.c
@@ -6,22 +6,26 @@ extern "C"
 {
 #endif
 
+/* ---- Types ---- */
+
 typedef struct PPE_MEMPOOL
 {
-    ppe_mp_allocate_vfn alloc;
-    ppe_mp_reallocate_vfn realloc;
-    ppe_mp_deallocate_vfn free;
+    ppe_mp_malloc_vfn malloc;
+    ppe_mp_realloc_vfn realloc;
+    ppe_mp_free_vfn free;
 } ppe_mempool_st;
 
-PPE_API ppe_mempool ppe_mp_create(ppe_mp_allocate_vfn restrict alloc, ppe_mp_reallocate_vfn restrict realloc, ppe_mp_deallocate_vfn restrict free)
+/* ---- Functions ---- */
+
+PPE_API ppe_mempool ppe_mp_create(ppe_mp_allocate_vfn restrict malloc, ppe_mp_realloc_vfn restrict realloc, ppe_mp_free_vfn restrict free)
 {
-    ppe_mempool mp = calloc(sizeof(1, ppe_mempool_st));
+    ppe_mempool mp = calloc(1, sizeof(ppe_mempool_st));
     if (! mp) {
         ppe_err_set(PPE_ERR_OUT_OF_MEMORY, NULL);
         return NULL;
     }
 
-    mp->alloc = alloc;
+    mp->malloc = malloc;
     mp->realloc = realloc;
     mp->free = free;
     return mp;
@@ -34,23 +38,68 @@ PPE_API void ppe_mp_destroy(ppe_mempool restrict mp)
     }
 }
 
-PPE_API void * ppe_mp_allocate(ppe_mempool_itf restrict itf, ppe_size size)
+PPE_API void * ppe_mp_malloc_from(ppe_mempool_itf restrict itf, ppe_size size)
 {
-    return itf ? itf->alloc(itf, size) : malloc(size);
+    return itf ? itf->malloc(itf, size) : malloc(size);
 }
 
-PPE_API void * ppe_mp_reallocate(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size)
+PPE_API void * ppe_mp_calloc_from(ppe_mempool_itf restrict itf, ppe_size num, ppe_size size)
 {
-    return itf ? itf->realloc(itf, size) : realloc(ptr, size);
+    void * p = NULL;
+    if (! itr) return calloc(num, size);
+
+    if ((p = itf->malloc(itf, num * size))) memset(p, 0, num * size);
+    return p;
 }
 
-PPE_API void ppe_mp_deallocate(ppe_mempool_itf restrict itf, void * restrict ptr)
+PPE_API void * ppe_mp_realloc_from(ppe_mempool_itf restrict itf, void * restrict ptr, ppe_size size)
+{
+    return itf ? itf->realloc(itf, ptr, size) : realloc(ptr, size);
+}
+
+PPE_API void ppe_mp_free_to(ppe_mempool_itf restrict itf, void * restrict ptr)
 {
     if (itf) {
         itf->free(itf, ptr); 
     } else {
         free(ptr);
     }
+}
+
+static ppe_mempool_itf s_mp_default = PPE_MP_GLOBAL_ALLOCATOR;
+
+PPE_API ppe_mempool_itf ppe_mp_set_default(ppe_mempool_itf restrict itf)
+{
+    ppe_mempool_itf t = s_mp_default;
+    s_mp_default = itf;
+    return t;
+}
+
+PPE_API ppe_mempool_itf ppe_mp_get_default(void)
+{
+    return s_mp_default;
+}
+
+/* ---- Wrappers ---- */
+
+PPE_API void * ppe_mp_malloc(ppe_size size)
+{
+    return ppe_mp_malloc_from(s_mp_default, size);
+}
+
+PPE_API void * ppe_mp_calloc(ppe_size num, ppe_size size)
+{
+    return ppe_mp_calloc_from(s_mp_default, num * size);
+}
+
+PPE_API void * ppe_mp_realloc(void * restrict ptr, ppe_size size)
+{
+    return ppe_mp_realloc_from(s_mp_default, ptr, size);
+}
+
+PPE_API void ppe_mp_free(void * restrict ptr)
+{
+    return ppe_mp_free_to(s_mp_default, ptr);
 }
 
 #ifdef __cplusplus
