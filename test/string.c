8fbe83141dbffa080648894fd84dc29d1f0c8cd7
LIANG Tao <amethyst.black@gmail.com>
2020-05-11 23:02:07 +0800

    TEST: Add test cases for the ppe_cs_substr().

--- test/string.c
+++ test/string.c
@@ -78,210 +78,128 @@ static void test_cs_create(void)
 
     s = ppe_cs_create("", 0);
     CU_ASSERT_PTR_NOT_NULL(s);
-    CU_ASSERT_EQUAL(s, ppe_cs_get_empty());
+    CU_ASSERT_PTR_EQUAL(s, ppe_cs_get_empty());
     CU_ASSERT_EQUAL(ppe_cs_size(s), 0);
 
     ppe_cs_destroy(s);
 
     s = ppe_cs_create("", 0);
     CU_ASSERT_PTR_NOT_NULL(s);
-    CU_ASSERT_EQUAL(s, ppe_cs_get_empty());
+    CU_ASSERT_PTR_EQUAL(s, ppe_cs_get_empty());
     CU_ASSERT_EQUAL(ppe_cs_size(s), 0);
 
     ppe_cs_destroy(s);
 
     s = ppe_cs_create(b, strlen(b));
     CU_ASSERT_PTR_NOT_NULL(s);
-    CU_ASSERT_NOT_EQUAL(s, ppe_cs_get_empty());
+    CU_ASSERT_PTR_NOT_EQUAL(s, ppe_cs_get_empty());
     CU_ASSERT_EQUAL(ppe_cs_size(s), strlen(b));
     CU_ASSERT_EQUAL(strncmp(s, b, strlen(b)), 0);
 
     s2 = ppe_cs_create(b, strlen(b));
     CU_ASSERT_PTR_NOT_NULL(s2);
-    CU_ASSERT_NOT_EQUAL(s2, ppe_cs_get_empty());
+    CU_ASSERT_PTR_NOT_EQUAL(s2, ppe_cs_get_empty());
     CU_ASSERT_EQUAL(ppe_cs_size(s2), strlen(b));
     CU_ASSERT_EQUAL(strncmp(s2, b, strlen(b)), 0);
-    CU_ASSERT_NOT_EQUAL(s2, s);
+    CU_ASSERT_PTR_NOT_EQUAL(s2, s);
     CU_ASSERT_EQUAL(strncmp(s2, s, ppe_cs_size(s)), 0);
 
     ppe_cs_destroy(s);
     ppe_cs_destroy(s2);
-}
+} /* test_cs_create */
 
-/*
-static void test_str_get_empty(void)
+static void test_cs_substr_for_using_new_string_mode(void)
 {
-    ppe_string str = ppe_str_get_empty();
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str)), 0);
-    CU_ASSERT_EQUAL(ppe_str_size(str), 0);
-}
+    ppe_cstr_c s = "This is a test line.";
+    ppe_cstr_c t = NULL;
+    ppe_cstr_c t2 = NULL;
+    
+    /* -- Test NEW-STRING MODE -- */
+    /* At the beginning. */
+    t = ppe_cs_substr(s, 0, 5, NULL, NULL, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_NOT_EQUAL(t, s);
+    CU_ASSERT_EQUAL(strncmp(t, s, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "This ", 5), 0);
 
-static void test_cs_clone(void)
+    /* At the middle. */
+    t2 = ppe_cs_substr(s, 10, 5, NULL, NULL, 0);
+    CU_ASSERT_PTR_NOT_NULL(t2);
+    CU_ASSERT_PTR_NOT_EQUAL(t2, s);
+    CU_ASSERT_PTR_NOT_EQUAL(t2, t);
+    CU_ASSERT_EQUAL(strncmp(t2, s + 10, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t2, "test ", 5), 0);
+
+    ppe_cs_destroy(t);
+    ppe_cs_destroy(t2);
+
+    /* At the end. */
+    t = ppe_cs_substr(s, 15, 5, NULL, NULL, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_NOT_EQUAL(t, s);
+    CU_ASSERT_EQUAL(strncmp(t, s + 15, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "line.", 5), 0);
+
+    ppe_cs_destroy(t);
+
+    /* Reach beyond the end. */
+    t = ppe_cs_substr(s, 17, 5, NULL, NULL, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_NOT_EQUAL(t, s);
+    CU_ASSERT_EQUAL(strncmp(t, s + 17, 3), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "ne.", 3), 0);
+
+    ppe_cs_destroy(t);
+} /* test_cs_substr_for_using_new_string_mode */
+
+static void test_cs_substr_for_using_fill_buffer_mode(void)
 {
-    const char * src = "TEST LINE";
-    ppe_string str = NULL;
-    ppe_string str2 = NULL;
+    ppe_cstr_c s = "This is a test line.";
+    ppe_cstr_c t = NULL;
+    ppe_char b[10];
+    ppe_size bsz = sizeof(b);
 
-    str = ppe_cs_clone("", 0);
-    CU_ASSERT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str)), 0);
-    CU_ASSERT_EQUAL(ppe_str_size(str), 0);
+    /* -- Test FILL-BUFFER MODE -- */
+    /* At the beginning. */
+    bsz = sizeof(b);
+    t = ppe_cs_substr(s, 0, 5, b, &bsz, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_EQUAL(t, b);
+    CU_ASSERT_EQUAL(strncmp(t, s, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "This ", 5), 0);
 
-    str = ppe_cs_clone(ppe_str_cstr(ppe_str_get_empty()), 0);
-    CU_ASSERT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str)), 0);
-    CU_ASSERT_EQUAL(ppe_str_size(str), 0);
+    /* At the middle. */
+    bsz = sizeof(b);
+    t = ppe_cs_substr(s, 10, 5, b, &bsz, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_EQUAL(t, b);
+    CU_ASSERT_EQUAL(strncmp(t, s + 10, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "test ", 5), 0);
 
-    str = ppe_cs_clone("", PPE_STR_AUTO_LENGTH);
-    CU_ASSERT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str)), 0);
-    CU_ASSERT_EQUAL(ppe_str_size(str), 0);
+    /* At the end. */
+    bsz = sizeof(b);
+    t = ppe_cs_substr(s, 15, 5, b, &bsz, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_EQUAL(t, b);
+    CU_ASSERT_EQUAL(strncmp(t, s + 15, 5), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "line.", 5), 0);
 
-    str = ppe_cs_clone(src, PPE_STR_AUTO_LENGTH);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str)), strlen(src));
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(src));
-    CU_ASSERT_EQUAL(strncmp(ppe_str_cstr(str), src, strlen(src)), 0);
+    /* Reach beyond the end. */
+    bsz = sizeof(b);
+    t = ppe_cs_substr(s, 17, 5, b, &bsz, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_EQUAL(t, b);
+    CU_ASSERT_EQUAL(strncmp(t, s + 17, 3), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "ne.", 3), 0);
 
-    str2 = ppe_cs_clone(src, strlen(src));
-    CU_ASSERT_NOT_EQUAL(str2, ppe_str_get_empty());
-    CU_ASSERT_NOT_EQUAL(str2, str);
-    CU_ASSERT_EQUAL(strlen(ppe_str_cstr(str2)), strlen(src));
-    CU_ASSERT_EQUAL(ppe_str_size(str2), strlen(src));
-    CU_ASSERT_EQUAL(strncmp(ppe_str_cstr(str), src, strlen(src)), 0);
-
-    ppe_mp_free(str2);
-    ppe_mp_free(str);
-}
-
-static void test_str_destroy(void)
-{
-    const char * src = "TEST LINE";
-    ppe_string str = NULL;
-
-    ppe_str_destroy(ppe_str_get_empty());
-
-    str = ppe_cs_clone(src, strlen(src));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-
-    ppe_str_destroy(str);
-}
-
-static void test_cs_join_2(void)
-{
-    const char * src1 = "TEST";
-    const char * src2 = "LINE";
-    const char * ret1 = "TEST LINE"; 
-    const char * ret2 = "TEST::LINE"; 
-    const char * ret3 = "TEST\tLINE"; 
-    const char * ret4 = "TESTLINE"; 
-    ppe_string str = NULL;
-
-    str = ppe_cs_join_2(" ", src1, strlen(src1), src2, strlen(src2));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret1));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret1), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join_2(" ", src1, PPE_STR_AUTO_LENGTH, src2, PPE_STR_AUTO_LENGTH);
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret1));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret1), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join_2("::", src1, strlen(src1), src2, PPE_STR_AUTO_LENGTH);
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret2));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret2), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join_2("\t", src1, PPE_STR_AUTO_LENGTH, src2, strlen(src2));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret3));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret3), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join_2("", src1, strlen(src1), src2, strlen(src2));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret4));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret4), 0);
-
-    ppe_str_destroy(str);
-}
-
-static void test_cs_concat_2(void)
-{
-    const char * src1 = "TEST";
-    const char * src2 = "LINE";
-    const char * src3 = "words";
-    const char * ret1 = "TESTLINE"; 
-    const char * ret2 = "TESTwords"; 
-    ppe_string str = NULL;
-
-    str = ppe_cs_concat_2(src1, strlen(src1), src2, strlen(src2));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret1));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret1), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_concat_2(src1, strlen(src1), src3, strlen(src3));
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret2));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret2), 0);
-
-    ppe_str_destroy(str);
-}
-
-static void test_cs_join(void)
-{
-    const char * src1 = "TEST";
-    const char * src2 = "LINE";
-    const char * src3 = "text";
-    const char src4 = {"sentence"};
-    const char * ret1 = "TESTLINEtext"; 
-    const char * ret2 = "text\tLINE\tTEST"; 
-    const char * ret3 = "sentence---TEST---text---LINE"; 
-    ppe_string str = NULL;
-
-    str = ppe_cs_join("", src1, strlen(src1), src2, strlen(src2), src3, strlen(src3), PPE_STR_ARG_END);
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret1));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret1), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join("\t", src3, strlen(src3), src2, strlen(src2), src1, strlen(src1), PPE_STR_ARG_END);
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret2));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret2), 0);
-
-    ppe_str_destroy(str);
-
-    str = ppe_cs_join("---", src4, strlen(src4), src1, strlen(src1), src3, strlen(src3), src2, strlen(src2), PPE_STR_ARG_END);
-    CU_ASSERT_PTR_NOT_NULL(str);
-    CU_ASSERT_NOT_EQUAL(str, ppe_str_get_empty());
-    CU_ASSERT_EQUAL(ppe_str_size(str), strlen(ret3));
-    CU_ASSERT_EQUAL(strcmp(ppe_str_cstr(str), ret3), 0);
-
-    ppe_str_destroy(str);
-}
-*/
+    /* Truncate the substring. */
+    bsz = sizeof(b);
+    t = ppe_cs_substr(s, 5, 10, b, &bsz, 0);
+    CU_ASSERT_PTR_NOT_NULL(t);
+    CU_ASSERT_PTR_EQUAL(t, b);
+    CU_ASSERT_EQUAL(strncmp(t, s + 5, 9), 0);
+    CU_ASSERT_EQUAL(strncmp(t, "is a test", 9), 0);
+} /* test_cs_substr_for_using_fill_buffer_mode */
 
 CU_TestInfo test_normal_cases[] = {
     {"test_cs_get_empty()", test_cs_get_empty},
@@ -290,6 +208,8 @@ CU_TestInfo test_normal_cases[] = {
     {"test_cs_compare()", test_cs_compare},
     {"test_cs_find()", test_cs_find},
     {"test_cs_create()", test_cs_create},
+    {"test_cs_substr_for_using_new_string_mode()", test_cs_substr_for_using_new_string_mode},
+    {"test_cs_substr_for_using_fill_buffer_mode()", test_cs_substr_for_using_fill_buffer_mode},
     CU_TEST_INFO_NULL
 };
 
